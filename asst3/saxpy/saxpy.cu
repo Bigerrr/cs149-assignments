876cc42fcf673a07d8c17c200186a82e7c46c880
Bigerrr <bigerrrchn@qq.com>
2025-05-25 08:21:08 +0000

    Finish Assign3 Part1

--- asst3/saxpy/saxpy.cu
+++ asst3/saxpy/saxpy.cu
@@ -75,6 +75,9 @@ void saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultar
     //
     // https://devblogs.nvidia.com/easy-introduction-cuda-c-and-c/
     //
+    cudaMalloc(&device_x, N * sizeof(float));
+    cudaMalloc(&device_y, N * sizeof(float));
+    cudaMalloc(&device_result, N * sizeof(float));
         
     // start timing after allocation of device memory
     double startTime = CycleTimer::currentSeconds();
@@ -82,16 +85,21 @@ void saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultar
     //
     // CS149 TODO: copy input arrays to the GPU using cudaMemcpy
     //
-
+    cudaMemcpy(device_x, xarray, N * sizeof(float), ::cudaMemcpyHostToDevice);
+    cudaMemcpy(device_y, yarray, N * sizeof(float), ::cudaMemcpyHostToDevice);
+    
    
     // run CUDA kernel. (notice the <<< >>> brackets indicating a CUDA
     // kernel launch) Execution on the GPU occurs here.
+    double kernelStartTime = CycleTimer::currentSeconds();
     saxpy_kernel<<<blocks, threadsPerBlock>>>(N, alpha, device_x, device_y, device_result);
+    cudaDeviceSynchronize(); // 等待GPU运行完毕
+    double kernelEndTime = CycleTimer::currentSeconds();
 
     //
     // CS149 TODO: copy result from GPU back to CPU using cudaMemcpy
     //
-
+    cudaMemcpy(resultarray, device_result, N * sizeof(float), ::cudaMemcpyDeviceToHost);
     
     // end timing after result has been copied back into host memory
     double endTime = CycleTimer::currentSeconds();
@@ -105,10 +113,19 @@ void saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultar
     double overallDuration = endTime - startTime;
     printf("Effective BW by CUDA saxpy: %.3f ms\t\t[%.3f GB/s]\n", 1000.f * overallDuration, GBPerSec(totalBytes, overallDuration));
 
+    double copyDuration = kernelStartTime - startTime;
+    double kernelDuration = kernelEndTime - kernelStartTime;
+
+    printf("Copy BW from CPU to GPU: %.3f ms\t\t[%.3f GB/s]\n", 1000.f * copyDuration, GBPerSec(2*N*sizeof(float), copyDuration));
+
+    printf("Kernel Process Time: %.3f ms\t\t\t[%.3f GB/s]\n", 1000.f * kernelDuration, GBPerSec(totalBytes, kernelDuration));
+    printf("-----------------------------------------------------------\n");
     //
     // CS149 TODO: free memory buffers on the GPU using cudaFree
     //
-    
+    cudaFree(device_x);
+    cudaFree(device_y);
+    cudaFree(device_result);
 }
 
 void printCudaInfo() {
